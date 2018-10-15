fd6777d2c2d360292df2e5b6b1ec246879596fbe
Christobal <geekarist@users.noreply.github.com>
2018-10-15 17:27:17 +0200

    Say 'hello user'

--- app/src/main/java/appauthlc/xebia/fr/appauthlivecoding/MainActivity.kt
+++ app/src/main/java/appauthlc/xebia/fr/appauthlivecoding/MainActivity.kt
@@ -1,12 +1,16 @@
 package appauthlc.xebia.fr.appauthlivecoding
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val userName = "Unknown"
+        mainHello.text = getString(R.string.main_hello_user, userName)
     }
 }
