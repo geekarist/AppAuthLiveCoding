48b62409541148a2ee1c272913f6dcb13d7e7740
Christobal <geekarist@users.noreply.github.com>
2018-10-25 17:32:12 +0200

    Return auth code then fetch name

--- app/src/main/java/fr/xebia/appauthlc/MainActivity.kt
+++ app/src/main/java/fr/xebia/appauthlc/MainActivity.kt
@@ -7,10 +7,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
-import net.openid.appauth.AuthorizationRequest
-import net.openid.appauth.AuthorizationService
-import net.openid.appauth.AuthorizationServiceConfiguration
-import net.openid.appauth.ResponseTypeValues
+import net.openid.appauth.*
 import java.io.PrintWriter
 import java.io.StringWriter
 
@@ -22,6 +19,13 @@ class MainActivity : AppCompatActivity(), FetchNameAsyncTask.Listener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        when (intent?.action) {
+            Intent.ACTION_MAIN -> requestAuthCode()
+            ACTION_RETURN_AUTH_CODE -> fetchName(intent)
+        }
+    }
+
+    private fun requestAuthCode() {
         val config = AuthorizationServiceConfiguration(
                 Uri.parse(BuildConfig.GOOGLE_AUTH_ENDPOINT),
                 Uri.parse(BuildConfig.GOOGLE_TOKEN_ENDPOINT)
@@ -32,11 +36,24 @@ class MainActivity : AppCompatActivity(), FetchNameAsyncTask.Listener {
                 ResponseTypeValues.CODE,
                 Uri.parse(BuildConfig.GOOGLE_REDIRECT_URI)
         ).setScope("profile").build()
-        val completedIntent = PendingIntent.getActivity(this, 0, Intent(), 0)
+        val intent = Intent(this, javaClass).setAction(ACTION_RETURN_AUTH_CODE)
+        val completedIntent = PendingIntent.getActivity(this, 0, intent, 0)
         authorizationService.performAuthorizationRequest(request, completedIntent)
+    }
 
-//        val authToken = "TODO"
-//        FetchNameAsyncTask(this).execute(authToken)
+    private fun fetchName(intent: Intent) {
+        val response = AuthorizationResponse.fromIntent(intent)
+        val error = AuthorizationException.fromIntent(intent)
+        val authState = AuthState(response, error)
+
+        response?.createTokenExchangeRequest()?.let { tokenRequest ->
+            authorizationService.performTokenRequest(tokenRequest) { response, ex ->
+                authState.update(response, ex)
+                authState.performActionWithFreshTokens(authorizationService) { accessToken, _, _ ->
+                    FetchNameAsyncTask(this).execute(accessToken)
+                }
+            }
+        }
     }
 
     override fun onFetchNameSuccess(displayName: String) {
@@ -53,5 +70,9 @@ class MainActivity : AppCompatActivity(), FetchNameAsyncTask.Listener {
     override fun onDestroy() {
         authorizationService.dispose()
         super.onDestroy()
+    }
+
+    companion object {
+        private const val ACTION_RETURN_AUTH_CODE = "ACTION_RETURN_AUTH_CODE"
     }
 }
