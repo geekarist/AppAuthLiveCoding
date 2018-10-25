1f566463b8bc7d36ffc0896dedd8bd2c9ce022a3
Christobal <geekarist@users.noreply.github.com>
2018-10-25 08:30:46 +0200

    Perform auth request

--- app/src/main/java/fr/xebia/appauthlc/MainActivity.kt
+++ app/src/main/java/fr/xebia/appauthlc/MainActivity.kt
@@ -1,18 +1,42 @@
 package fr.xebia.appauthlc
 
+import android.app.PendingIntent
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import net.openid.appauth.AuthorizationRequest
+import net.openid.appauth.AuthorizationService
+import net.openid.appauth.AuthorizationServiceConfiguration
+import net.openid.appauth.ResponseTypeValues
 import java.io.PrintWriter
 import java.io.StringWriter
 
 class MainActivity : AppCompatActivity(), FetchNameAsyncTask.Listener {
+
+    private val authorizationService by lazy { AuthorizationService(this) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        FetchNameAsyncTask(this).execute("TODO")
+        val config = AuthorizationServiceConfiguration(
+                Uri.parse(BuildConfig.GOOGLE_AUTH_ENDPOINT),
+                Uri.parse(BuildConfig.GOOGLE_TOKEN_ENDPOINT)
+        )
+        val request = AuthorizationRequest.Builder(
+                config,
+                BuildConfig.GOOGLE_CLIENT_ID,
+                ResponseTypeValues.CODE,
+                Uri.parse(BuildConfig.GOOGLE_REDIRECT_URI)
+        ).setScope("profile").build()
+        val completedIntent = PendingIntent.getActivity(this, 0, Intent(), 0)
+        authorizationService.performAuthorizationRequest(request, completedIntent)
+
+//        val authToken = "TODO"
+//        FetchNameAsyncTask(this).execute(authToken)
     }
 
     override fun onFetchNameSuccess(displayName: String) {
@@ -24,5 +48,10 @@ class MainActivity : AppCompatActivity(), FetchNameAsyncTask.Listener {
         val writer = StringWriter()
         e.printStackTrace(PrintWriter(writer))
         Log.e(javaClass.simpleName, "Error fetching name: $writer")
+    }
+
+    override fun onDestroy() {
+        authorizationService.dispose()
+        super.onDestroy()
     }
 }
